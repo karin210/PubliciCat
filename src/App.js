78fd10406f2616ac415e78099f3417a016eea7ed
Karlos <karin_ar72@hotmail.com>
2023-01-24 09:56:53 -0600

    Activaciones Empresariales section created, section's header created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import './App.css';
 import Header from './components/Header';
+import SectionH1 from './components/SectionH1';
 function App() {
  
 
   return (
+    // home page
     <div className="App">
      <Header />
      <div className='slides'></div>
+    {/* Activaciones Empresariales page */}
+     <div className='ActEmp'>
+      <SectionH1 
+        text = "Activaciones Empresariales"
+      />
+     </div>
     </div>
   );
 }
---
+++ src/components/SectionH1.js
@@ -0,0 +1,9 @@
+
+
+function SectionH1(props){
+    return(
+        <h1 className="section-header">{props.text}</h1>
+    )
+}
+
+export default SectionH1;
