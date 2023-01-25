9f761bf715974dcca64e05f7301ffb0390ae33eb
Karlos <karin_ar72@hotmail.com>
2023-01-25 16:09:09 -0600

    Nosotros page team members pic added

--- src/App.js
+++ src/App.js
@@ -118,7 +118,8 @@ function App() {
       <SectionH1 
           text = "Nosotros"
         />
-      <img className="nosotrosImgG" src=""/>
+      <img className="nosotrosImgG" src="https://images.unsplash.com/photo-1543269865-cbf427effbad?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"/>
+      <span className="nosotrosImgTxt">Equipo de PubliCat</span>
     </div>
     </div>
   );
