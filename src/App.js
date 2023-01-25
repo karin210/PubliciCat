de4653ded6e2cbd27e20c4714d1062694797ae17
Karlos <karin_ar72@hotmail.com>
2023-01-25 17:52:20 -0600

    team members added

--- src/App.js
+++ src/App.js
@@ -122,11 +122,24 @@ function App() {
         <img className="nosotrosImg" src="https://images.unsplash.com/photo-1543269865-cbf427effbad?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"/>
         <span className="nosotrosImgTxt">Equipo de PubliciCat</span>
       </div>
-      <p className="nosotrosLeyenda">En PubliCat somos un equipo con amplia experiencia en proyectos publicitarios. También contamos con servicio de renta de equipos de audio profesional y luces LED para eventos</p>
+      <p className="nosotrosLeyenda">En PubliciCat somos un equipo con amplia experiencia en proyectos publicitarios. También contamos con servicio de renta de equipos de audio profesional y luces LED para eventos</p>
+      <h2>Team</h2>
       <section id='teamMembers'>
-        <div className="nosotrosItem">
+        <div className="teamItems">
           <img className="nosotrosTeamImg" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1887&q=80"/>
           <span className="membersInfo">Anna Abbey</span>
+        </div>
+        <div className="teamItems">
+          <img className="nosotrosTeamImg" src="https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"/>
+          <span className="membersInfo">Andy Keiffer</span>
+        </div>
+        <div className="teamItems">
+          <img className="nosotrosTeamImg" src="https://images.unsplash.com/photo-1524504388940-b1c1722653e1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"/>
+          <span className="membersInfo">Cindy Allin</span>
+        </div>
+        <div className="teamItems">
+          <img className="nosotrosTeamImg" src="https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"/>
+          <span className="membersInfo">Carla López</span>
         </div>
       </section>
     </div>
