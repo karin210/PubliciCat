2393199120350776d5c6ecf28af0ec8c4e085e7a
Karlos <karin_ar72@hotmail.com>
2023-01-26 18:56:08 -0600

    sectionH1 padding top modified

--- src/components/SectionH1.js
+++ src/components/SectionH1.js
@@ -3,7 +3,7 @@ import "../styles/sectionH1.css";
 function SectionH1(props){
     return(
         <h1 className="section-header" id={props.id}>{props.text}</h1>
-    )
+    );
 }
 
 export default SectionH1;
