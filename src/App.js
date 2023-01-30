7e3d75da4c7ccbc90748e259b54bd526e8b4b212
Karlos <karin_ar72@hotmail.com>
2023-01-29 20:21:49 -0600

    setted media queries to the Renta de equipo section

--- src/App.js
+++ src/App.js
@@ -62,7 +62,7 @@ function App() {
       <SectionH1 
         text = "Renta de equipo"
       />
-      <div className="rentEquipo-items">
+      <div className="rentEquipoItems">
         <RenEquipoItem
         img = "https://images.unsplash.com/photo-1516873240891-4bf014598ab4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
         text = "DJ" 
