b6b83e45a32dcb5b8af142da40a0d8bf861f23d3
Karlos <karin_ar72@hotmail.com>
2023-02-08 20:46:11 -0600

    hisrorias item's modal modified and functionality setted

--- src/components/Historia.js
+++ src/components/Historia.js
@@ -1,9 +1,26 @@
 import "../styles/historia.css";
+import { useRef, useState } from "react";
 
 function Historia(props){
+
+    const [modal, setModal] = useState(true);
+
+
+    const historia = useRef(null);
+    const historiaTxt = useRef(null);
+    function showTxt (){
+      if(modal){
+      historiaTxt.current.style.display = "block";
+      setModal(!modal);
+      } else {
+          historiaTxt.current.style.display = "none";
+          setModal(modal);
+        }
+    }
+
     return(
-        <div className="historia" id={props.id}>
-            <p className="histTxt">{props.txt}</p>
+        <div ref={historia} onClick={showTxt} className="historia" id={props.id}>
+            <p ref={historiaTxt} className="histTxt">{props.txt}</p>
         </div>
     );
 }
