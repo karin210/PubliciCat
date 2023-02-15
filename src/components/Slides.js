ae3bc7ea89d84071ffe58b0870d1fddda983553b
Karlos <karin_ar72@hotmail.com>
2023-02-15 16:38:09 -0600

    style: add media queries to the size of the slides

--- src/components/Slides.js
+++ src/components/Slides.js
@@ -1,6 +1,6 @@
 import '../styles/slides.css';
 // import Swiper core and required modules
-import { Navigation, Pagination, Scrollbar, A11y, EffectFade } from 'swiper';
+import { Navigation, Pagination, Scrollbar, A11y, EffectFade, Autoplay } from 'swiper';
 
 import { Swiper, SwiperSlide } from 'swiper/react';
 
@@ -10,13 +10,15 @@ import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
 import 'swiper/css/effect-fade';
+import 'swiper/css/autoplay';
 
 export default () => {
   return (
     <Swiper
       // install Swiper modules
-      modules={[Navigation, Pagination, Scrollbar, A11y, EffectFade]}
+      modules={[Navigation, Pagination, Scrollbar, A11y, EffectFade, Autoplay]}
       loop={true}
+      autoplay={false}
       effect="fade"
       spaceBetween={50}
       slidesPerView={1}
@@ -25,11 +27,16 @@ export default () => {
       onSwiper={(swiper) => console.log(swiper)}
       onSlideChange={() => console.log('slide change')}
     >
-      <SwiperSlide><img src='https://corporacionimagen.com/wp-content/uploads/2019/09/05-09-Partido-Guatemala-vs-Anguila-265.jpg'/>
+      <SwiperSlide>
+        <img alt='Activaciones Empresariales' src='https://corporacionimagen.com/wp-content/uploads/2019/09/05-09-Partido-Guatemala-vs-Anguila-265.jpg'/><span>Activaciones Empresariales</span>
       </SwiperSlide>
-      <SwiperSlide><img src='https://www.softzone.es/app/uploads-softzone.es/2021/08/sonido-windows.jpg'/></SwiperSlide>
-      <SwiperSlide><img src='https://images.unsplash.com/photo-1517816428104-797678c7cf0c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'/></SwiperSlide>
-      <SwiperSlide><img src='https://images.unsplash.com/photo-1598826867442-9ef9e2527b1e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'/></SwiperSlide>
+      <SwiperSlide>
+        <img alt='Audio' src='https://images.unsplash.com/photo-1528724747537-1634153aa3c8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'/>
+        <span>Audio PRO</span></SwiperSlide>
+      <SwiperSlide>
+        <img alt='Marketing' src='https://images.unsplash.com/photo-1598826867442-9ef9e2527b1e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'/>
+        <span>Marketing</span>
+      </SwiperSlide>
     </Swiper>
   );
 };
