d0b75623fe2e789c2288138b8050d9c40f54735b
Karlos <karin_ar72@hotmail.com>
2023-02-15 10:22:01 -0600

    update: add image slides

--- src/components/Slides.js
+++ src/components/Slides.js
@@ -25,11 +25,11 @@ export default () => {
       onSwiper={(swiper) => console.log(swiper)}
       onSlideChange={() => console.log('slide change')}
     >
-      <SwiperSlide><img src='https://scontent.fmlm4-1.fna.fbcdn.net/v/t31.18172-8/14543790_874661719331013_8906457848622013803_o.jpg?_nc_cat=109&ccb=1-7&_nc_sid=9267fe&_nc_ohc=f_sWj9ghmLkAX840CDJ&_nc_ht=scontent.fmlm4-1.fna&oh=00_AfDY70wKSuJiqsDCtJDdNUeBfxgHdTieiZb0_JTj6p-95Q&oe=63FE4812'/>
+      <SwiperSlide><img src='https://corporacionimagen.com/wp-content/uploads/2019/09/05-09-Partido-Guatemala-vs-Anguila-265.jpg'/>
       </SwiperSlide>
-      <SwiperSlide><img src='https://www.androidguys.com/wp-content/uploads/2015/12/Wave-Wallpapers-1.jpg'/></SwiperSlide>
-      <SwiperSlide><img src='https://www.shutterstock.com/image-illustration/triangle-solid-black-golden-illustration-260nw-1862937556.jpg'/></SwiperSlide>
-      <SwiperSlide><img src='https://images5.alphacoders.com/112/1123013.jpg'/></SwiperSlide>
+      <SwiperSlide><img src='https://www.softzone.es/app/uploads-softzone.es/2021/08/sonido-windows.jpg'/></SwiperSlide>
+      <SwiperSlide><img src='https://images.unsplash.com/photo-1517816428104-797678c7cf0c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'/></SwiperSlide>
+      <SwiperSlide><img src='https://images.unsplash.com/photo-1598826867442-9ef9e2527b1e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'/></SwiperSlide>
     </Swiper>
   );
 };
